e06f94360ddba132f5b67b8e0ec8cc9f631960eb
haluu102 <ha.luu102@hcmut.edu.vn>
2024-04-28 03:23:48 +0700

    remove LessonModule from CourseModule

--- Backend/nestjs/src/domains/courses/courses.module.ts
+++ Backend/nestjs/src/domains/courses/courses.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { CourseService } from './courses.service';
 import { CourseController } from './courses.controller';
 import { PrismaModule } from 'src/services/prisma/prisma.module';
-import { LessonModule } from '../lessons/lessons.module';
 
 @Module({
-  imports: [PrismaModule, LessonModule],
+  imports: [PrismaModule],
   controllers: [CourseController],
   providers: [CourseService],
   exports: [CourseService],
