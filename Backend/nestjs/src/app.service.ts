592e0cee31f281d2b8dae48551c0690f37f8ee33
haluu102 <ha.luu102@hcmut.edu.vn>
2024-05-15 19:57:49 +0700

    .

--- Backend/nestjs/src/app.service.ts
+++ Backend/nestjs/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   connectServer(): string {
-    return 'AI learning system';
+    return '<h1> AI learning system </h1>';
   }
 }
